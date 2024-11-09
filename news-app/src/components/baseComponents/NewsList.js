b20a23e2b1dd5392d483a128f37d7bb38bd558aa
AksshayaK <aksshaya.k@gmail.com>
2024-11-08 20:22:40 -0800

    styling change
    moved the styling changes in the news aggregator

--- news-app/src/components/baseComponents/NewsList.js
+++ news-app/src/components/baseComponents/NewsList.js
@@ -1,29 +1,24 @@
-import React,{useState} from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const NewsList = () => {
-   const {articles,loading,error}  = useSelector((state) => state.news);
-  //  const [articles,setArticles] = useState([]);
-  //  const [loading,setLoading] = useState(false);
-  //  const [error,setError] = useState("");
-    if(loading)
-    {
-      return <div>Loading...</div>
-    }
-    if(error)
-    {
-      return <div>Error: {error}</div>
-    }
+  const { articles, loading, error } = useSelector((state) => state.news);
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>Error: {error}</div>
+  }
 
-    return (<div style={{ margin: '10px' }} className='border'>
-        {articles.map((article) => (
-            <div key={article.url} className="flex">
-                <h5>{article.tile}</h5>
-                <p>{article.description ? article.description.substring(0, 150) : ""}</p>
-                <a href={article.url} target="_blank">Read more</a>
-            </div>
-        ))}
-    </div>);
+  return (<div style={{ margin: '10px' }} className='border'>
+    {articles?.length > 0 && articles.map((article) => (
+      <div key={article.url} className="flex">
+        <h5>{article.tile}</h5>
+        <p>{article.description ? article.description.substring(0, 150) : ""}</p>
+        <a href={article.url} target="_blank">Read more</a>
+      </div>
+    ))}
+  </div>);
 }
 
 export default NewsList;
