897885c52a596b1b6f58882276a517f0354e0b8d
AksshayaK <aksshaya.k@gmail.com>
2024-11-09 16:35:36 -0800

    Update Dino.js
    made changes to update the score

--- dino-game/src/components/Dino.js
+++ dino-game/src/components/Dino.js
@@ -5,9 +5,10 @@ const Dino = () => {
     const dinoRef = useRef();
     const cactusRef = useRef();
     const [score,setScore] = useState(0);
+    const [cactusPassed,setCactusPassed] = useState(false);
 
     const jump = () => {
-        if(!!dinoRef.current && dinoRef.current.classList !="jump")
+        if(!!dinoRef.current && dinoRef.current.classList.contains("jump"))
         {
             dinoRef.current.classList.add("jump");
             setTimeout(() => dinoRef.current.classList.remove("jump"),300);
@@ -18,13 +19,22 @@ const Dino = () => {
         const isAlive = setInterval(() => {
             let dinoTop = parseInt(getComputedStyle(dinoRef.current).getPropertyValue("top"));
             let cactusLeft = parseInt(getComputedStyle(cactusRef.current).getPropertyValue("left"));
+            let dinoLeft = parseInt(getComputedStyle(dinoRef.current).getPropertyValue("left"));
+            let cactusRight = parseInt(getComputedStyle(cactusRef.current).getPropertyValue("right"));
             if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
                 alert("Game Over! Your score: " + score);
                 setScore(0);
             }
-            else {
-                setScore(score + 1);
+            if(cactusRight < dinoLeft && !cactusPassed)
+            {
+                setScore(prev => prev + 1);
+                setCactusPassed(true);
             }
+            if(cactusLeft > window.innerWidth)
+            {
+                setCactusPassed(false);
+            }
+           
         }, 10);
         return () => clearInterval(isAlive);
     },[]);
